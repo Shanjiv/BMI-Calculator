9626d24a56d5f230b06ea48cf9b0eb17182f9e83
Shanjiv <shanjiv@hotmail.com>
2016-05-11 00:13:58 +0200

    Start of BMI-Calculator

---
+++ BMI-Index/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BMI-Index
+//
+//  Created by Shanseevan Kanagaratnam on 11.05.16.
+//  Copyright © 2016 Shanseevan Kanagaratnam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
